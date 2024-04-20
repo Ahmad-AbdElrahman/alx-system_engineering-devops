60d4374c06f652250569f5824f8fefc2cd1ad72f
Ahmed <ahmed@ahmed>
2024-04-20 05:53:43 +0200

    3

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,15 +1,7 @@
-exec { 'install ruby':
-  command => '/usr/bin/apt-get install -y python3'
-}
-
-
-package { 'python3':
-  ensure => 'installed',
-  before => Exec['install python3']
-}
+# Install a package using Puppet
 
 package { 'flask':
-  ensure  => 'installed',
-  before  => Exec['pip3 install flask'],
-  require => Package['python3-pip3']
+  ensure   => '2.1.0',
+  provider => 'pip3'
+  require  => Package['python3-pip3'],
 }
